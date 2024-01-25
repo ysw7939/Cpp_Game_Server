623edbe4bb259426d2d5a7389646f55c0adecf83
yangsuwon <yangsuwon@gmail.com>
2024-01-25 15:59:12 +0900

    Lock-Free Stack

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,8 +1,45 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <Windows.h>
+#include <future>
+#include "ConcurrentQueue.h"
+#include "ConcurrentStack.h"
+
+LockQueue<int32> q;
+LockStack<int32> s;
+
+void Push()
+{
+	while (true)
+	{
+		int32 value = rand() % 100;
+		q.Push(value);
+
+		this_thread::sleep_for(10ms);
+	}
+}
+
+void Pop()
+{
+	while (true)
+	{
+		int32 data = 0;
+		if (q.TryPop(OUT data))
+			cout << data << endl;
+	}
+}
+
 
 int main()
 {
-	HelloWorld();
+	thread t1(Push);
+	thread t2(Pop);
+	thread t3(Pop);
+
+	t1.join();
+	t2.join();
+	t3.join();
 }
